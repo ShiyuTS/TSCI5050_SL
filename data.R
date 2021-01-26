00b05dbf7c1f96fc9dd3985f33200a43786ce74d
SL <lis9@livemail.uthscsa.edu>
2021-01-26 10:07:46 -0600

    addressing HW comments 1-26-2021

--- data.R
+++ data.R
@@ -1,21 +1,20 @@
 dat0 <- import('C:/Users/shiyu/Downloads/fghm_heart_study_mortality_2018_v1.csv')
 
 #Question 1: install packages
-install.packages("devtools")
-install.packages("dplyr")
-install.packages("ggplot2")
-install.packages("pander")
-install.packages("table1")
-install.packages("broom")
-install.packages("synthpop")
-install.packages("rmarkdown")
+library("devtools")
+library("dplyr")
+library("ggplot2")
+library("pander")
+library("table1")
+library("broom")
+library("synthpop")
+library("rmarkdown")
 #import dat0 from local desktop
 dat0 <- import('C:/Users/shiyu/Downloads/fghm_heart_study_mortality_2018_v1.csv')
 #Question 2
 ##create odd_index with odd-numbered rows in dat0
 odd_index <- dat0[c(TRUE,FALSE),]
 ##selec numeric columns from odd_index using "select_if" function in "dplyr"
-library("dplyr")
 dat1 <- odd_index %>% select_if(is.numeric)
 #Question 3
 ##create even_index with even-numbered rows in dat0 
@@ -24,8 +23,6 @@ even_index <- dat0[c(FALSE,TRUE),]
 dat2 <- even_index[c("id", "chd", "sex", "obese", "hypertension")]
 #Question 4
 ##I explored the "filter" function in dplyr and tested in dat0 to filter out participants less than or equal to 60 years old but obese.
-?dplyr
-?filter
 young_age <- filter(dat0, age <= 60)
 View(young_age)
 young_age_obese <- filter(young_age, obese=="Obese")
