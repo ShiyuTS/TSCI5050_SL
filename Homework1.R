f1da3838c277519a4aa0033b4c7c4aa49e38400d
SL <lis9@livemail.uthscsa.edu>
2021-02-09 10:09:27 -0600

    homework 1

---
+++ Homework1.R
@@ -0,0 +1,31 @@
+#Question 1: install packages
+install.packages("devtools")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("pander")
+install.packages("table1")
+install.packages("broom")
+install.packages("synthpop")
+install.packages("rmarkdown")
+#import dat0 from local desktop
+dat0 <- import('C:/Users/shiyu/Downloads/fghm_heart_study_mortality_2018_v1.csv')
+#Question 2
+##create odd_index with odd-numbered rows in dat0
+odd_index <- dat0[c(TRUE,FALSE),]
+##selec numeric columns from odd_index using "select_if" function in "dplyr"
+library("dplyr")
+dat1 <- odd_index %>% select_if(is.numeric)
+#Question 3
+##create even_index with even-numbered rows in dat0 
+even_index <- dat0[c(FALSE,TRUE),]
+##i don't know how to use "select_if" to select non-numeric columns, so I used c() to create dat2
+dat2 <- even_index[c("id", "chd", "sex", "obese", "hypertension")]
+#Question 4
+##I explored the "filter" function in dplyr and tested in dat0 to filter out participants less than or equal to 60 years old but obese.
+?dplyr
+?filter
+young_age <- filter(dat0, age <= 60)
+View(young_age)
+young_age_obese <- filter(young_age, obese=="Obese")
+View(young_age_obese)
+
--- simulation.R
+++ simulation.R
@@ -8,5 +8,11 @@ dat0 <- import("../TSCI 5050 Tues AM/fghm_heart_study_mortality_2018_v1.csv")
 #add codebook
 dat0cb <- codebook.syn(dat0)
 
+#add simulation
+sim0 <- syn(dat0)
+
+#sim1 <- sdc(sim0,dat0,label='SIM',rm.replicated.uniques = T)
+
 # how to get rid of a column in a data.frame:
 #FOO$BAR <- NULL
+
