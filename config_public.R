da8ea2b80bcf997e076e08e9023e4d46ffcf96f8
SL <lis9@livemail.uthscsa.edu>
2021-02-23 13:04:29 -0600

    2-23-2021 class Part 2

---
+++ config_public.R
@@ -0,0 +1,3 @@
+# this is the simulated version of the data file
+datafile0 <- 'data/week3.csv';
+if(file.exists('config.R')) source('config.R');
--- data.R
+++ data.R
@@ -1,20 +1,17 @@
-source("config.R")
+source("config_public.R")
 library("rio")
-#import dat0 from local desktop
+library('ggplot2')
+library("survival")
 dat0 <- import(datafile0)
-#Question 2
-##create odd_index with odd-numbered rows in dat0
-odd_index <- dat0[c(TRUE,FALSE),]
-##selec numeric columns from odd_index using "select_if" function in "dplyr"
-dat1 <- odd_index %>% select_if(is.numeric)
-#Question 3
-##create even_index with even-numbered rows in dat0 
-even_index <- dat0[c(FALSE,TRUE),]
-##i don't know how to use "select_if" to select non-numeric columns, so I used c() to create dat2
-dat2 <- even_index[c("id", "chd", "sex", "obese", "hypertension")]
-#Question 4
-##I explored the "filter" function in dplyr and tested in dat0 to filter out participants less than or equal to 60 years old but obese.
-young_age <- filter(dat0, age <= 60)
-View(young_age)
-young_age_obese <- filter(young_age, obese=="Obese")
-View(young_age_obese)
+#create new variable
+#c_event <- 'NAME OF YOUR EVENT COLUMN'
+#c_time <- 'NAME OF THE TIME COLUMN'
+dat0$c_event <- dat0$chd == 'CHD'
+dat0$c_time <- dat0$pt_yrs
+table(dat0$c_event)
+#Quick way to summarize numeric data:
+hist(dat0$c_time)
+ggplot(dat0,aes(x=c_time,fill=c_event)) + geom_bar()
+ggplot(dat0,aes(x=c_time,fill=c_event)) + geom_boxplot()
+ggplot(dat0,aes(x=c_time, y=c_event)) + geom_violin()
+ggplot(dat0,aes(x=c_time, y=c_event, fill=c_event)) + geom_violin()
